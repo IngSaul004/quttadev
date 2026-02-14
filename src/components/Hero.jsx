149ad769bc60bb1391b27dc1557878af7d1dac89
IngSaul004 <ing.saul.rivera004@gmail.com>
2026-02-13 21:48:39 -0600

    se le agrego funcionamiento al boton del hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,23 @@
 import "./Hero.css";
 import logo from "../assets/QUTTADEV 22.png";  
+import { useState } from "react";
+
 function Hero() {
+const [isOpen, setIsOpen] = useState(false);
+
+  const handleScroll = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+      setIsOpen(false); // Cierra el menú en móvil
+    }
+  };
 return (
 <section className="hero" id="inicio">
     <img src={logo} alt="Logo de Quttadev" className="hero-logo" />
     <h1>Desarrollo Web Profesional</h1>
     <p>Diseño y desarrollo sitios web modernos, rápidos y funcionales para potenciar tu presencia digital.</p>
-    <button>Ver servicios</button>
+    <button onClick={() => handleScroll("servicios")}>Ver servicios</button>
 </section>
 )
 }
